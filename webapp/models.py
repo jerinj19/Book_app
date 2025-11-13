d9c2621bd4ab1548754cce37127a8c798b3aa544
jerinj19 <jerinj19>
2025-11-13 10:48:29 +0530

    created cart model and edited in single.html

--- webapp/models.py
+++ webapp/models.py
@@ -13,3 +13,12 @@ class Contactdb(models.Model):
     Contact_city=models.CharField(max_length=20,null=True,blank=True)
     Enquiry_type=models.CharField(max_length=20,null=True,blank=True)
     Contact_message=models.TextField(max_length=50,null=True,blank=True)
+
+class Cart(models.Model):
+    Book_name=models.CharField(max_length=25,null=True,blank=True)
+    username=models.CharField(max_length=30,null=True,blank=True)
+    qty=models.IntegerField(null=True,blank=True)
+    price=models.IntegerField(null=True,blank=True)
+    total_price=models.IntegerField(null=True,blank=True)
+    Book_image=models.ImageField(upload_to="Cart_books",null=True,blank=True)
+
