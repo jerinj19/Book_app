ae1741318f56a245411124aa397ba3d8b08e9572
jerinj19 <jerinj19>
2025-12-04 23:41:28 +0530

    edited in urls in webapp for deployment

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path
-from webapp import views
-urlpatterns=[
-    path('homepage/',views.homepage,name="homepage"),
-    path('contactpage/',views.contactpage,name="contact"),
-    path('about/',views.aboutpage,name="about"),
-    path('popular/',views.popularbooks,name="popular"),
-    path('filter/<b_id>/',views.filtred_books,name="filter"),
-    path('single/<int:book_id>/',views.single_book,name="single_book"),
-    path('signup/',views.signup,name="signup"),
-    path('savesignup/',views.saveuser,name="save"),
-    path('login/',views.login,name="login"),
-    path('userlogin/',views.userlogin,name="userlogin"),
-    path('userlogout/',views.userlogout,name="logout"),
-    path('savecontact/',views.saveContact,name="contact")
+from . import views
 
-
+urlpatterns = [
+    path('', views.homepage, name='homepage'),  # <- empty string '' matches '/'
+    path('contactpage/', views.contactpage, name='contact'),
+    path('about/', views.about, name='about'),
+    path('popular/', views.popular, name='popular'),
+    path('filter/<b_id>/', views.filter, name='filter'),
+    path('single/<int:book_id>/', views.single_book, name='single_book'),
+    path('signup/', views.signup, name='signup'),
+    path('savesignup/', views.save, name='save'),
+    path('login/', views.login, name='userlogin'),
+    path('userlogin/', views.userlogin, name='userlogin'),
+    path('userlogout/', views.userlogout, name='logout'),
+    path('savecontact/', views.savecontact, name='contact'),
 ]
